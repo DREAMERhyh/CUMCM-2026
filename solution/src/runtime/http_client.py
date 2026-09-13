75a460c238bb5985d108ae3cd4424b4c7115aea8
diego3893 <diegozcx@foxmail.com>
2026-09-13 18:06:01 +0800

    清理文件

--- solution/src/runtime/http_client.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility import; the canonical implementation is ``sim.client``."""
-
-from sim.client import HttpRobotClient
-from sim.protocol import build_request_payload
-
-__all__ = ["HttpRobotClient", "build_request_payload"]
